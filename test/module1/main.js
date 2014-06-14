3fff6315b4d6a5071508b2beb612c991f6ae6820
weistn <torben.weis@gmail.com>
2014-06-14 16:50:45 +0200

    Removed output files

--- test/module1/main.js
+++
@@ -1,4 +0,0 @@
-var __runtime = require('./runtime.js');
-console.log('Hello Gismo, from module1');
-console.log('NEW OPERAND');
-//# sourceMappingURL=main.js.map
--- test/module2/main.js
+++
@@ -1,5 +0,0 @@
-var __runtime = require('./runtime.js');
-var module1 = __runtime.__import('module1');
-console.log('I am module 2');
-console.log('NEW OPERAND');
-//# sourceMappingURL=main.js.map
