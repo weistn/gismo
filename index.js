002c91550cad5a635768b58c140840543b1c26b7
weistn <torben.weis@gmail.com>
2014-06-16 01:18:49 +0200

    Command line interface

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-var parser = require('./parser.js');
+var program = require('commander');
+var compiler = require('./compiler.js');
+var path = require('path');
+var fs = require('fs');
 
-function dodummy() {
-	console.log("Hallo App");
-	parser.foo();
-	return 42;
+var pkg = JSON.parse(fs.readFileSync('../package.json', 'utf8'));
+
+program
+  .version(pkg.version)
+  .usage('[options] <module_path ...>')
+  .parse(process.argv);
+
+for(var i = 0; i < program.args.length; i++) {
+	var arg = path.resolve(program.args[i]);
+	try {
+		var c = new compiler.Compiler(arg);
+		c.compileModule();
+	} catch(err) {
+		console.log(err.toString());
+	}
 }
-
-exports.dodummy = dodummy;
